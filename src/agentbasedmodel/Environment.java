e5bceec288d2ffa8156a15160bcbf03b1406f1ca
Clayton Pruitt <clay10pruitt@gmail.com>
2019-10-31 13:27:26 -0400

    Added constructor and equals() method to Environment. Updated type of population field in Environment to be a Collection instead of a List. More generic => more flexibility.

--- src/agentbasedmodel/Environment.java
+++ src/agentbasedmodel/Environment.java
@@ -1,10 +1,19 @@
 package agentbasedmodel;
 
-import java.util.List;
+import java.util.Collection;
 
 public class Environment {
 
-    private List<Agent> population;
+    // the collection of Agents currently in the environment
+    private Collection<Agent> population;
+
+    /**
+     * Initializes the Enivornment with the given collection of Agents.
+     * @param population pre-determined list of Agents
+     */
+    public Environment(Collection<Agent> population){
+        this.population = population;
+    }
 
     /**
      * Adds the given Agent to the population of the Environment.
@@ -29,4 +38,23 @@ public class Environment {
         return this.population.size();
     }
 
+    /**
+     * Two Environments are equal if their populations are equal.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        // An Environment is equal to itself.
+        if (obj == this){
+            return true;
+        }
+
+        // An Environment cannot be equal to a non-Environment.
+        if (!(obj instanceof Environment)){
+            return false;
+        }
+
+        // Two Environments are equal if their populations are equal.
+        Environment e = (Environment) obj;
+        return (this.population.equals(e.population));
+    }
 }
