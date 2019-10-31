6c7903c58cfe81382815b005e87880e63e841a26
Clayton Pruitt <clay10pruitt@gmail.com>
2019-10-31 14:19:27 -0400

    Updated Time to use separate hour and minute variables (as opposed to tracking both within one int variable). Implemented constructor for Time.

--- src/agentbasedmodel/Time.java
+++ src/agentbasedmodel/Time.java
@@ -2,22 +2,45 @@ package agentbasedmodel;
 
 public class Time {
 
-    private int time;
-    private int minTime;
-    private int MaxTime;
+    // the current hour (between 0 (inclusive) and 24 (exclusive))
+    private int hour;
+    // the current minute (between 0 (inclusive) and 60 (exclusive))
+    private int minute;
+
+    // the values for which hour and minute will, upon reaching, revert to zero
+    private int maxHour = 24;
+    private int maxMinute = 60;
 
     /**
-     * Advances the time by the given amount and ensures that we stay within the defined bounds of time.
-     * @param amount amount of time to advance by
+     * Constructor for Time.
+     * @param hour initial hour
+     * @param minute initial minute
      */
-    public void advanceTime(int amount){
+    public Time(int hour, int minute){
+        this.hour = hour;
+        this.minute = minute;
     }
 
     /**
-     * @return time
+     * Advances the time by the given amount and ensures that we stay below the maximum time.
+     * @param hours amount of hours to advance by
+     * @param minutes amount of minutes to advance by
      */
-    public int getTime(){
-        return this.time;
+    public void advanceTime(int hours, int minutes){
+        hour = ((hour + hours) % maxHour);
+        minute = ((minute + minutes) % maxMinute);
     }
+
+    /**
+     * @return hour
+     */
+    public int getHour(){
+        return this.hour;
+    }
+
+    /**
+     * @return minute
+     */
+    public int getMinute() { return this.minute; }
 
 }
