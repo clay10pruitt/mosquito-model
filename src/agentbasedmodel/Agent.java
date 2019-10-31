12d27cc7bde41c103e29cd2b9f56d9f1e81a7575
Clayton Pruitt <clay10pruitt@gmail.com>
2019-10-31 13:19:27 -0400

    Updated Agent documentation and added constructor, equals(), and toString() methods.

--- src/agentbasedmodel/Agent.java
+++ src/agentbasedmodel/Agent.java
@@ -2,7 +2,16 @@ package agentbasedmodel;
 
 public class Agent {
 
+    // the String associated with this Agent
     private String id;
+
+    /**
+     * Agent constructor.
+     * @param id the identification string of this agent
+     */
+    public Agent(String id){
+        this.id = id;
+    }
 
     /**
      * @return id
@@ -11,6 +20,28 @@ public class Agent {
         return this.id;
     }
 
-    //TODO: Implement equals() and toString().
+    @Override
+    public String toString() {
+        return ("agent#" + id);
+    }
 
+    /**
+     * Two Agents are equivalent if their IDs are the same.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        // an Agent will always be equivalent to itself
+        if (obj == this){
+            return true;
+        }
+
+        // an Agent cannot equal a non-Agent
+        if (!(obj instanceof  Agent)){
+            return false;
+        }
+
+        // two Agents are equal if their IDs are equal
+        Agent a = (Agent) obj;
+        return (this.id.equals(a.id));
+    }
 }
