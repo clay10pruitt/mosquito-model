e4c1915efc2c1749ae01ae40d09889e214d1593e
Clayton Pruitt <clay10pruitt@gmail.com>
2019-11-03 18:21:04 -0500

    Implemented simple, one agent simulation. Scheduling works fine. Still need to test the Mosquito.

--- src/simulation/Simulation.java
+++ src/simulation/Simulation.java
@@ -1,7 +1,53 @@
 package simulation;
 
+import agentbasedmodel.Agent;
+import agentbasedmodel.Report;
+import agentbasedmodel.Time;
+import mosquitomodel.*;
+
+import java.util.Collection;
+import java.util.LinkedHashSet;
+
 public class Simulation {
 
-    //TODO: Figure out what this class does.
+    private static Collection<Agent> createParkVisitors(){
+
+        Collection<Agent> parkVisitors = new LinkedHashSet<>();
+
+        // create Bob
+        ParkVisitorBiology bobBiology = new ParkVisitorBiology(44, 16000, 16000, 0);
+        Schedule bobSchedule = new Schedule(new Time(17, 0), new Time(19, 0));
+        ParkVisitor bob = new ParkVisitor("Bob", bobBiology, bobSchedule);
+
+        // add Bob to the collection of visitors
+        parkVisitors.add(bob);
+
+        return parkVisitors;
+    }
+
+    /**
+     * Main method for the simulation.
+     * @param args
+     */
+    public static void main(String[] args){
+
+        // create the agents
+        Collection<Agent> parkVisitors = createParkVisitors();
+
+        // initialize objects needed for simulation
+        Mosquito mosquito = new Mosquito(17500);
+        Park park = new Park(mosquito);
+        ParkManager parkManager = new ParkManager(park, new Time(0, 15));
+        parkManager.seedEnvironment(parkVisitors);
+        SimulationOperator simulationOperator = new SimulationOperator(parkManager, 96);
+
+        // run simulation
+        simulationOperator.runSimulation();
+        Report data = simulationOperator.getData();
+
+        // print report
+        System.out.println(data);
+
+    }
 
 }
