11f44fc74688019aa2f58d490c2dc2e2f4ab8cc5
Clayton Pruitt <clay10pruitt@gmail.com>
2019-11-03 17:58:44 -0500

    Implemented runSimulation() in SimulationOperator. Added constructor and getData() to SimulationOperator.

--- src/simulation/SimulationOperator.java
+++ src/simulation/SimulationOperator.java
@@ -2,16 +2,50 @@ package simulation;
 
 import agentbasedmodel.EnvironmentManager;
 import agentbasedmodel.Report;
+import agentbasedmodel.Time;
+import mosquitomodel.ParkVisitor;
+import mosquitomodel.ParkVisitorBiology;
+import mosquitomodel.Schedule;
+
+import java.util.LinkedHashSet;
 
 public class SimulationOperator {
 
+    // private fields
     private Report data;
+
+    // properties
     private EnvironmentManager environmentManager;
+    private int numberOfCyclesToRun;
 
-    public void runSimulation(){
-
+    /**
+     * Constructor for SimulationOperator.
+     * @param environmentManager: the Simulation's EnvironmentManager
+     * @param numberOfCyclesToRun: the amount of cycles to run the EnvironmentManager through
+     */
+    public SimulationOperator(EnvironmentManager environmentManager, int numberOfCyclesToRun){
+        this.environmentManager = environmentManager;
+        this.numberOfCyclesToRun = numberOfCyclesToRun;
     }
 
-    //TODO: Getter for data.
+
+    /**
+     * Runs the simulation by putting the EnvironmentManager through the number of cycles specified in the constructor.
+     */
+    public void runSimulation(){
+        for (int c = 0; c < numberOfCyclesToRun; c++){
+            Report cycleReport = environmentManager.runCycle();
+            data.add(cycleReport);
+        }
+    }
+
+
+    /**
+     * Gets the Report contained within this SimulationOperator.
+     * @return data
+     */
+    public Report getData(){
+        return this.data;
+    }
 
 }
