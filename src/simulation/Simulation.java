c364bdfbbff00c4bc16291ee8781a5691a92b3e4
Clayton Pruitt <clay10pruitt@gmail.com>
2019-11-06 11:51:57 -0500

    Simulation config params are now read from a file.

--- src/simulation/Simulation.java
+++ src/simulation/Simulation.java
@@ -7,9 +7,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedHashSet;
+import java.util.*;
 
 public class Simulation {
 
@@ -23,6 +21,48 @@ public class Simulation {
         return new BufferedReader(new FileReader(file));
     }
 
+
+    /**
+     * Parses configuration parameters from the give=n file.
+     * @param reader BufferedReader for reading the file
+     * @return a Map with param names as keys and their values as the map's values
+     * @throws IOException
+     */
+    private static Map<String, Object> parseConfig(BufferedReader reader) throws IOException {
+
+        Map<String, Object> config = new HashMap<>();
+
+        // get data from file
+        String line;
+        while ((line = reader.readLine()) != null){
+            String[] paramAndValue = line.split("=");
+            String param = paramAndValue[0].replaceAll("\\s", "");
+            String value = paramAndValue[1].replaceAll("\\s", "");
+            config.put(param, value);
+        }
+
+        // interpret data
+        for (String param : config.keySet()){
+            Object value = config.get(param);
+            if (param.equals("cycleLength")) {
+                value = new Time((String) value);
+            } else if (param.equals("numberOfCycles")){
+                value = Integer.parseInt((String) value);
+            } else if (param.equals("initialTime")){
+                value = new Time((String) value);
+            } else if (param.equals("mosquitoActivationTime")){
+                value = new Time((String) value);
+            } else if (param.equals("mosquitoDeactivationTime")) {
+                value = new Time((String) value);
+            }
+            config.put(param, value);
+        }
+
+        return config;
+
+    }
+
+
     /**
      * Parses a .csv file to create Agents. Lines should be in the following form:
      *      [id, time in, time out, age, max hearable frequency]
@@ -31,7 +71,6 @@ public class Simulation {
      * @throws IOException
      */
     private static Collection<Agent> parseParkVisitorsFromFile(BufferedReader reader) throws IOException {
-
         // get data from file
         String line = reader.readLine();
         ArrayList<String[]> abomimatrix = new ArrayList<>();
@@ -55,8 +94,24 @@ public class Simulation {
      */
     public static void main(String[] args){
 
-        // create buffered reader to read input file
+        // create buffered reader to read config file
         BufferedReader reader = null;
+        try {
+            reader = createBufferedReaderForFile("resources/config.txt");
+        } catch (FileNotFoundException e){
+            return;
+        }
+
+        // retrieve configuration parameters
+        Map<String, Object> config = null;
+        try {
+            config = parseConfig(reader);
+        } catch (IOException e){
+            return;
+        }
+
+        // create another buffered reader to read agent input file
+        reader = null;
         try {
             reader = createBufferedReaderForFile("resources/agents.csv");
         } catch (FileNotFoundException e){
@@ -72,10 +127,17 @@ public class Simulation {
         }
 
         Park e = new Park(new Mosquito(17500));
-        EnvironmentManager em = new ParkManager(e, new Time(0, 30));
+        EnvironmentManager em = new ParkManager(
+                e,
+                (Time) config.get("cycleLength"),
+                (Time) config.get("initialTime"),
+                (Time) config.get("mosquitoActivationTime"),
+                (Time) config.get("mosquitoDeactivationTime"));
         em.seedEnvironment(agents);
 
-        SimulationOperator so = new SimulationOperator(em, 48);
+        SimulationOperator so = new SimulationOperator(
+                em,
+                (Integer) config.get("numberOfCycles"));
 
         so.runSimulation();
         System.out.println(so.getData());
